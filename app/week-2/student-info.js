1e74366297c598222659530196b693a5b3806264
RyanHowieGH <ryan.howie@edu.sait.ca>
2025-01-17 14:15:19 -0700

    Update landing page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Ryan Howie</h1>
+            <p><Link href="http://github.com/RyanHowieGH/cprg306-assignments/">Github Repository</Link></p>
+            </main>
+            );
+        }
