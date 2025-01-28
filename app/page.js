3568f44c7c22eb00b6b0792d8f4db0199d6271d5
RyanHowieGH <ryan.howie@edu.sait.ca>
2025-01-28 15:21:34 -0700

    Add week three page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ import Link from 'next/link';
 export default function Page() {
   return (
   <main>
-<h1>CPRG 306: Web Development 2 - Assignments</h1>
-<p><Link href="/week-2">Week 2</Link></p>
+<h1 className="font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+<h2><Link href="/week-2">Week 2</Link></h2>
+<h2><Link href="/week-3">Week 3</Link></h2>
 </main>
   );
 }
