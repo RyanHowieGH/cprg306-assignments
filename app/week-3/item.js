02c1c8e0ba46276b83dcf9be4c945e1e67292a61
RyanHowieGH <ryan.howie@edu.sait.ca>
2025-01-28 15:32:52 -0700

    Add styling

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,9 +2,9 @@ export default function Item ({name, quantity, category}){
     return (
     <main>
         <ul>
-            <li className="px-4 bg-slate-200 max-w-sm">{name}</li>
+            <li className="px-4 bg-slate-200 max-w-sm capitalize font-bold">{name}</li>
             <li className="px-4 bg-slate-200 max-w-sm">{quantity}</li>
-            <li className="px-4 bg-slate-200 max-w-sm">{category}</li>
+            <li className="px-4 bg-slate-200 max-w-sm mb-4">{category}</li>
         </ul>
     </main>
     )
