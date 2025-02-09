589785518324a96ab9fd4dac92afdf08c5578d23
RyanHowieGH <ryan.howie@edu.sait.ca>
2025-02-08 19:03:54 -0700

    Add week 4

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Page() {
 <h1 className="font-bold">CPRG 306: Web Development 2 - Assignments</h1>
 <h2><Link href="/week-2">Week 2</Link></h2>
 <h2><Link href="/week-3">Week 3</Link></h2>
+<h2><Link href="/week-4">Week 4</Link></h2>
 </main>
   );
 }
