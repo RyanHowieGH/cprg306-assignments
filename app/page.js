beec65e7155780a24f4a85715cd09ecd0818d1fb
RyanHowieGH <159562023+RyanHowieGH@users.noreply.github.com>
2025-02-24 14:55:32 -0700

    Add week 5 stuff

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Page() {
 <h2><Link href="/week-2">Week 2</Link></h2>
 <h2><Link href="/week-3">Week 3</Link></h2>
 <h2><Link href="/week-4">Week 4</Link></h2>
+<h2><Link href="/week-5">Week 5</Link></h2>
 </main>
   );
 }
