825325fbc1fec1b2fa76111c9637c24da239e08a
RyanHowieGH <159562023+RyanHowieGH@users.noreply.github.com>
2025-02-28 14:01:18 -0700

    Starting week 6

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Page() {
 <h2><Link href="/week-3">Week 3</Link></h2>
 <h2><Link href="/week-4">Week 4</Link></h2>
 <h2><Link href="/week-5">Week 5</Link></h2>
+<h2><Link href="/week-6">Week 6</Link></h2>
 </main>
   );
 }
---
+++ app/week-6/page.js
@@ -0,0 +1,11 @@
+import ItemList from './item-list.js';
+
+export default function Page(){
+
+    
+    return (
+        <main className="bg-gray-800">
+            <ItemList />
+        </main>
+    )
+}
